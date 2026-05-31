faf346ee77e5c71ced248602fb511c505cd72b69
ak2563718 <ak2563718@gmail.com>
2026-05-31 09:45:03 +0530

    Day 10 commit

--- src/redux/feature/auth/authAction.ts
+++ src/redux/feature/auth/authAction.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const baseUri = `http://localhost:3000/api`
-export const userLogin = createAsyncThunk<unknown,void>(
+export const userLogin = createAsyncThunk(
     'user/Login',
     async( user, {rejectWithValue})=>{
         try {
@@ -12,9 +12,60 @@ export const userLogin = createAsyncThunk<unknown,void>(
             })
             return data;
         } catch (error) {
-           if(error.response){
-             return rejectWithValue(error.response.data.message);
+           if(axios.isAxiosError(error)){
+            return rejectWithValue(error.response?.data?.message)
+           }
+        }
+    }
+)
+
+export const userSignup = createAsyncThunk(
+    'user/Signup',
+    async( info , {rejectWithValue})=>{
+        try {
+            const {data}= await axios.post(`${baseUri}/signup`,info,{
+                withCredentials:true,
+                headers:{'Content-Type':'application/json'},
+            })
+            return data;
+        } catch (error) {
+            if(axios.isAxiosError(error)){
+                return rejectWithValue(error.response?.data?.message)
+            }
+        }
+    }
+)
+
+export const userLogout = createAsyncThunk(
+    'user/logout',
+    async( _ , {rejectWithValue})=>{
+        try {
+            const { data } = await axios.get(`${baseUri}/logout`,{
+                headers:{'Content-Length':"application/json"},
+                withCredentials:true,
+            })
+            return data;
+        } catch (error) {
+           if(axios.isAxiosError(error)){
+            return rejectWithValue(error.response?.data?.message)
            } 
+        }
+    }
+)
+
+export const check_session = createAsyncThunk(
+    'user/check-session',
+    async( _ , {rejectWithValue})=>{
+        try {
+            const { data } = await axios.get(`${baseUri}/check-session`,{
+                headers:{'Content-Type':"application/json"},
+                withCredentials:true,
+            })
+            return data;
+        } catch (error) {
+            if(axios.isAxiosError(error)){
+                return rejectWithValue(error.response?.data?.message)
+            }
         }
     }
 )
--- src/redux/feature/auth/authSlice.ts
+++ src/redux/feature/auth/authSlice.ts
@@ -1,11 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { userLogin, userLogout, userSignup } from "./authAction";
 
-const initialState ={
-    user:[],
-    loading:false,
-    message:null,
-    error:null,
-    islogin:false,
+interface AuthState {
+    user: any;
+    loading: boolean;
+    message: string | null;
+    error: string | null;
+    islogin: boolean;
+}
+
+const initialState: AuthState = {
+    user: {},
+    loading: false,
+    message: null,
+    error: null,
+    islogin: false,
 }
 
 const authSlice = createSlice({
@@ -13,7 +22,50 @@ const authSlice = createSlice({
     initialState,
     reducers:{},
     extraReducers:(builder)=>{
+            // 1. signup user
+            builder.addCase(userSignup.pending,(state)=>{
+                state.loading = true;
+                state.error = null;
+                state.message = null;
+            }).addCase(userSignup.fulfilled,(state,action)=>{
+                state.loading = false;
+                state.message = action.payload.message;
+                state.user = action.payload.user;
+            }).addCase(userSignup.rejected,(state,action)=>{
+                state.loading = false;
+                state.error = action.payload;
+            });
 
+            // 2. Login user
+            builder.addCase(userLogin.pending,(state)=>{
+                state.loading = true;
+                state.message = null;
+                state.error = null;
+            }).addCase(userLogin.fulfilled,(state,action)=>{
+                state.loading = false;
+                state.user = action.payload.user;
+                state.message = action.payload.message;
+                state.islogin = true;
+            }).addCase(userLogin.rejected,(state,action)=>{
+                state.loading = false;
+                state.error = action.payload;
+                state.islogin = false;
+            });
+
+            // 3. Logout user
+            builder.addCase(userLogout.pending,(state)=>{
+                state.loading = true;
+                state.message = null;
+                state.error = null;
+            }).addCase(userLogout.fulfilled,(state,action)=>{
+                state.loading = false;
+                state.islogin = false;
+                state.message = action.payload.message;
+                state.user = {};
+            }).addCase(userLogout.rejected,(state,action)=>{
+                state.loading = false;
+                state.error = action.payload;
+            })
     }
 })
 
